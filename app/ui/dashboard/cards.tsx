665f52065736623d9fddcd87a11615ee0c627f09
Mike Rains <mike@officemike.com>
2024-11-28 04:59:39 -0600

    change labels

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -20,8 +20,8 @@ export default async function CardWrapper() {
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
 
-      <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
+      <Card title="Collected Spock" value={totalPaidInvoices} type="collected" />
+      <Card title="Pending Kirk" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
