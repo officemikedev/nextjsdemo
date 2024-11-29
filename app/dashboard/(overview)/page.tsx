bb636841ecfad6bf42d0d0230e4f6703813ea7fd
OfficeMike Mike <169008807+officemikedev@users.noreply.github.com>
2024-11-28 19:28:30 -0600

    Update page.tsx

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -29,7 +29,7 @@ export default async function Page({
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Suspense key={ticks+1} fallback={<CardsSkeleton/>}>
-        <CardWrapper  userid={ticks}/>
+        <CardWrapper  userid={ticks.toString()}/>
         </Suspense>
         {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
         <Card title="Pending" value={totalPendingInvoices} type="pending" />
